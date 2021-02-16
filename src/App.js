55a002fd980c754a433e2040d86211e7f767c0ef
Jonasz Jałocha <jonasz.jalocha@gmail.com>
2021-02-16 15:51:21 -0800

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Power Engineering
         </a>
       </header>
     </div>
