c06900e44e510222f76b5824fff81f9c02e3da8b
Jonasz Jałocha <jonasz.jalocha@gmail.com>
2021-02-19 09:02:45 -0800

    Add custom-made json with conductors - continue work on data import.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,26 +1,8 @@
-import React, {Component, useEffect} from 'react'
+import React, {Component } from 'react'
 
-// const getData = () => {
-//     fetch('PowerConductors', {
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'Accept': 'application/json'
-//             }
-//         }
-//     ).then(function (response) {
-//         console.log(response)
-//         return response.json();
-//     }).then(function (myJson) {
-//         console.loge(myJson);
-//
-//     });
-// }
-//
-// useEffect(() => {getData()}, [])
-//
+
 
 class Calculator extends Component {
-
 
     render() {
         const a = 2;
